94aa7cb6752de1c72122101fd94603438d8db3ca
Zorzan99 <solotovzds@hotmail.com>
2024-01-15 00:02:01 -0300

    Feat: Funcoes

---
+++ lib/1_funcoes/1_funcoes.dart
@@ -0,0 +1,9 @@
+void main() {
+  final resultado = somaInteiros(10, 10);
+  print('Resultado $resultado');
+}
+
+int somaInteiros(int a, int b) {
+  print('Soma dos inteiros ($a, $b)');
+  return a + b;
+}
